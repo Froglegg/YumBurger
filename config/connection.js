2bf67e6b288a6ddbb6c8ca70059d2eeb08e2aa64
froglegg <hayescrowley@gmail.com>
2019-09-04 15:50:37 -0400

    messing with heroku db stuff

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.CLEARDB_DATABASE_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+}
+else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
